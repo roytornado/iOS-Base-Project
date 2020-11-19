72f88948a33be82c77737bc61bc1f84c3f9d081e
Roy Ng <royng@redso.com.hk>
2020-11-19 11:39:44 +0000

    Initial Commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Roy Ng on 19/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
